8923075de7949d05a49e4065a085fdf72e78749c
Jakub Petrík <jaku0584@stud.kea.dk>
2018-04-23 09:34:44 +0200

    added more classes/views&controllers

---
+++ HomeCare/Controllers/CaretakerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeCare.Controllers
+{
+    public class CaretakerController : Controller
+    {
+        // GET: Caretaker
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
