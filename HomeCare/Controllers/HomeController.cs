9ee3cf07a0cc08b152b161540bb98fd4515a8f99
GerliP <gerl0016@stud.kea.dk>
2018-04-23 16:06:03 +0200

    dbConnection added

--- HomeCare/Controllers/HomeController.cs
+++ HomeCare/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HomeCare.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +11,8 @@ namespace HomeCare.Controllers
     {
         public ActionResult Login()
         {
-
+            DBConnect dbConnect = new DBConnect();
+            dbConnect.OpenConnection();
             return View();
         }
         //[HttpGet]
